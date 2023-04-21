d66181034481883b4bdda027d2dcc3713ac949ea
peter <peterekwere04@gmail.com>
2023-04-21 03:41:42 -0700

    i just started the printf project

---
+++ _printf.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _printf - This is a printf type function without some format specifiers
+ * @format: is a pointer to a char array
+ * Returns: a string with any includes data or variable
+ */
+int _printf(const char *format, ...)
+{
+
+}
