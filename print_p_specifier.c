decca063b3bb9bcb44fa882869acf8ff02f8d560
Joy Emeh <joysmart30@gmail.com>
2023-04-26 09:34:26 -0700

    Print p

---
+++ print_p_specifier.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *  print_P - this is a function used to write out a reverse string to the
+ * @arguments: a list of arguments passed to the string
+ * Return: count
+ */
+
+int print_P(va_list arguments)
+{
+	void *ptr = va_arg(arguments, void *);
+
+	char buffer[1024];
+	int i;
+	int count = 0;
+
+	sprintf(buffer, "%p", ptr);
+
+	for (i = 0; buffer[i] != '\0'; i++)
+	{
+		write(1, buffer[i], 1);
+		count++;
+	}
+	return (count);
+}
